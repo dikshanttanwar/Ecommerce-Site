271e3e0e19e9079e362de4d8f1377fffba8cccc6
Dikshant Tanwar <dikshant00tanwar@gmail.com>
2024-12-01 21:17:57 +0530

    Added all the files together

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import Context from "../Utility/Context.jsx";
-import {BrowserRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 import { ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <Context>
       <App />
       <ToastContainer/>
     </Context>
-  </BrowserRouter>
+  </HashRouter>
 );
